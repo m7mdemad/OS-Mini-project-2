767ffce8155528a197283f1c15090ee74aa234f1
Demha <commandos_valentino@yahoo.com>
2018-12-10 14:55:52 +0200

    edit

--- disk.cpp
+++ disk.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
+
 #define upID  1997
 #define downID 2020
-
-struct slot{
-    char mtext[64];
-};
 
 struct stream{
     long mtype;
     int clks;
     char[7];
     char data[64];
-    
 };
 
 class Disk_Process{
-    slot slots[10];
+    string slots[10];
     int free_count;
     key_t upStream;
     key_t downStream;
@@ -26,12 +24,7 @@ public:
 };
 
 Disk_Process::Disk_Process(){
-    for(int i=0;i<10;i++) slot[i] = "";
     free_count = 10;
     upStream = msgget(upID, IPC_CREAT|0644);
     downStream = msgget(downID, IPC_CREAT|0644);
 }
-
-
-
-
